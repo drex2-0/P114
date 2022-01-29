8aa6e6387c9dd8f97992bf6ab66855f2801aa06d
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-01-29 12:31:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,31 @@ function preload(){
 function setup(){
     canvas=createCanvas(400,400);
     canvas.center();
-}
-function draw(){
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 
 }
-function snap(){
-    save("myclownImage.jpeg");
+function modelLoaded(){
+    console.log("Model is Loaded");
 }
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x= "+ results[0].pose.nose.x);
+        console.log("nose y= "+ results[0].pose.nose.y);
+    }
+}
+function draw(){
+    image(video,0,0,400,400);
+}
+function snap(){
+    save("myMustachEImage.jpeg");
+}
+
+
 
